2f8d79bfd8b26a409d44fdab1b1485d8cab3a716
Daniel Thoma <dthoma@konaktiva.tu-darmstadt.de>
2020-08-20 14:23:43 +0200

    fix tailwind production config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,27 @@
 // const { colors } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  plugins: [
-    require('@tailwindcss/custom-forms')
-  ],
-  theme: {
-      extend: {
-        colors: {
-          primary: '#F56565',
-          secondary: '#F7FAFC',
-          bgprimary: '#2D3748',
-          bgsecondary: '#1A202C'
+    plugins: [
+        require('@tailwindcss/custom-forms')
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#F56565',
+                secondary: '#F7FAFC',
+                bgprimary: '#2D3748',
+                bgsecondary: '#1A202C'
+            }
         }
-      }
-  }
+    },
+    purge: {
+        enabled: process.env.NODE_ENV === 'production',
+        content: [
+            'components/**/*.vue',
+            'layouts/**/*.vue',
+            'pages/**/*.vue',
+            'plugin/**/*.js',
+            'nuxt.config.js'
+        ]
+    }
 }
